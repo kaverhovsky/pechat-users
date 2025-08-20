2708f1a25380c53530972e2249a5b53c0cd3233e
kaverhovsky <ver17hovsk@gmail.com>
2025-08-20 19:34:38 +0300

    pulled spf13/viper, added config

---
+++ internal/pkg/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Postgres struct {
+		DSN string `mapstructure:"dsn"`
+	} `mapstructure:"postgres"`
+
+	Logger struct {
+		Level string `mapstructure:"level"`
+		Mode  string `mapstructure:"mode"`
+	} `mapstructure:"logger"`
+}
+
+// LoadConfig TODO добавить opts для тестов
+func LoadConfig(path string) (*Config, error) {
+	viper.SetConfigFile(path)
+	viper.AutomaticEnv()
+	// сделать viper.SetEnvPrefix через opts
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to read config: %w", err)
+	}
+
+	var c Config
+	if err := viper.Unmarshal(&c); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	return &c, nil
+}
